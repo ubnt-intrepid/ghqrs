df17b4019ca054ccb5ebaa73e3122a8c54e5cbe3
Yusuke Sasaki <yusuke.sasaki.nuem@gmail.com>
2016-09-27 03:40:36 +0900

    fix build.rs && add CI configuration

--- build.rs
+++ build.rs
@@ -1,10 +1,17 @@
 extern crate clap;
 
+use std::path::Path;
+use std::fs::OpenOptions;
 use clap::Shell;
 
 include!("src/cli.rs");
 
 fn main() {
-  let mut app = build_cli();
-  app.gen_completions("ghqrs", Shell::Bash, env!("OUT_DIR"));
+  let mut file = OpenOptions::new()
+    .write(true)
+    .create(true)
+    .open(Path::new(env!("CARGO_MANIFEST_DIR")).join("_ghqrs").to_str().unwrap())
+    .unwrap();
+
+  build_cli().gen_completions_to("ghqrs", Shell::Bash, &mut file);
 }
