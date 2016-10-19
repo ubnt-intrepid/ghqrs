66a1bbf2b758080c2314553039a670695ea4c689
Yusuke Sasaki <yusuke.sasaki.nuem@gmail.com>
2016-10-19 10:50:12 +0900

    simplify the procedure of pattern match

--- src/main.rs
+++ src/main.rs
@@ -6,30 +6,25 @@ mod cli;
 fn main() {
   let matches = cli::build_cli().get_matches();
 
-  let exitcode = if let Some(ref s) = matches.subcommand_name() {
-    let ref matches = matches.subcommand_matches(s).unwrap();
-    match *s {
-      "get" => {
-        let projects = matches.values_of("project").unwrap().map(ToOwned::to_owned).collect();
-        let pull = matches.is_present("pull");
-        let depth =
-          matches.value_of("depth").map(ToOwned::to_owned).map(|d| d.parse::<i32>().unwrap());
-        ghqrs::command_get(projects, pull, depth)
-      }
-      "list" => {
-        let exact = matches.is_present("exact");
-        let format = matches.value_of("format").unwrap_or("default");
-        let query = matches.value_of("query").map(ToOwned::to_owned);
-        ghqrs::command_list(exact, format, query)
-      }
-      "root" => {
-        let all = matches.is_present("all");
-        ghqrs::command_root(all)
-      }
-      _ => unreachable!(),
+  let exitcode = match matches.subcommand() {
+    ("get", Some(m)) => {
+      let projects = m.values_of("project").unwrap().map(ToOwned::to_owned).collect();
+      let pull = m.is_present("pull");
+      let depth = m.value_of("depth").map(|ref d| d.parse::<i32>().unwrap());
+      ghqrs::command_get(projects, pull, depth)
     }
-  } else {
-    unreachable!()
+    ("list", Some(m)) => {
+      let exact = m.is_present("exact");
+      let format = m.value_of("format").unwrap_or("default");
+      let query = m.value_of("query").map(ToOwned::to_owned);
+      ghqrs::command_list(exact, format, query)
+    }
+    ("root", Some(m)) => {
+      let all = m.is_present("all");
+      ghqrs::command_root(all)
+    }
+    (_, _) => unreachable!(),
   };
+
   std::process::exit(exitcode);
 }
